8fdd8fd57e06c02170c670dfb34d6cb8775c4f84
not-uzual <ujjwalsahu19@gmail.com>
2025-10-07 18:12:39 +0530

    all get request creation

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import socket
 import queue
 import threading
 import concurrent.futures
+import os
+from datetime import datetime, timezone
 
 class server:
     _SERVER = '127.0.0.1'
@@ -13,7 +15,7 @@ class server:
     def __init__(self):
         try:
             if len(sys.argv) > 1:
-                self._PORT = int(sys.argv[1])
+                self._PORT = int(sys.argv[1]) #who knows if it is something else
             if len(sys.argv) > 2:
                 self._SERVER = sys.argv[2]
             if len(sys.argv) > 3:
@@ -30,28 +32,119 @@ class server:
     
     def log_message(self, message):
         with self._print_lock:
-            print(message)
+            print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {message}")
+            print()
             
             
-    def parse_http_request(self, data):
+    def create_Response(self, req_method, resource_path):
+        if req_method == 'GET':
+            if resource_path == '/' or resource_path.split('.')[1] == 'html':
+                resource_path = 'index.html'
+                
+                path = os.path.join('.', 'resources' ,resource_path)
+                self.log_message(f'path i got::: {path}')
+                
+                try:
+                    with open(path, 'r') as file:
+                        http_content = file.read()
+                except FileNotFoundError:
+                    http_content = "<html><body><p>File not found</p></body></html>"
+                    self.log_message("[ERROR] file not found!")
+                    
+                http_header = (
+                    "HTTP/1.1 200 OK\r\n"
+                    "Content-Type: text/html\r\n"
+                    f"Content-Length: {len(http_content)}\r\n"
+                    "charset=utf-8\r\n"
+                    "\r\n"
+                )
+                
+                http_response = http_header + http_content
+                return http_response.encode(self._FORMAT)
+            
+            elif resource_path.split('.')[1] in ['png', 'jpg', 'txt']:
+                path = os.path.join('.', 'resources', resource_path)
+                try:
+                    if resource_path.split('.')[1] == 'txt':
+                        with open(path, 'r') as file:
+                            http_content = file.read()
+                            http_content = http_content.encode(self._FORMAT) 
+                    else:
+                        with open(path, 'rb') as file:
+                            http_content = file.read()
+                except FileNotFoundError:
+                    http_content = "<html><body><p>File not found</p></body></html>"
+                    self.log_message("[ERROR] file not found!")
+                
+                http_header = (
+                    "HTTP/1.1 200 OK\r\n"
+                    "Content-Type: application/octet-stream\r\n"
+                    f"Content-Length: {len(http_content)}\r\n"
+                    f"Content-Disposition: attachment; filename={resource_path}\r\n"
+                    f"Date: {datetime.now(timezone.utc).strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
+                    "Server: Multi-threaded HTTP Server\r\n"
+                    "Connection: keep-alive\r\n"
+                    "\r\n"
+                )
+                
+                http_response = http_header.encode(self._FORMAT) + http_content
+                return http_response    
+                
+                
+            
+    def parse_http_request(self, req_data):
     
-        if not data:
+        if not req_data:
             return None, {}
         
-        lines = data.decode(self._FORMAT).split("\r\n")
+        lines = req_data.decode(self._FORMAT).split("\r\n")
         
-        req_method = lines[0]
+        if not lines:
+            return None, {}
         
+        req_line = lines[0]
         headers = {}
         
         for line in lines:
             if not line:
-                continue
+                continue #to get params
             if ':' in line:
                 key, value = line.split(":", 1)
                 headers[key.strip().lower()] = value.strip()
         
-        return req_method, headers
+        return req_line, headers
+    
+    def handle_Request(self, clientSocket, clientAddr):
+                            
+        self.log_message(f"[Thread-{self._THREADPOOL - self._thread_semaphore._value}] Connection from {clientAddr[0]}:{clientAddr[1]}")
+        try:
+            request_data = clientSocket.recv(1024)
+        
+            req_line, headers = self.parse_http_request(request_data)
+            
+            connection_type = headers.get('connection', '').lower()
+            persistent = 'keep-alive' in connection_type
+            
+            self.log_message(f"Request: {req_line}")
+            self.log_message(f"Host validation: {headers.get('host', '').lower()} ✓")
+
+            self.log_message(f"Connection: {connection_type}")
+            
+            req_line = req_line.split(" ")
+            method = req_line[0]
+            req_file = req_line[1]
+            
+            if req_file != '/':
+                req_file = req_line[1].split('/')[1]
+            
+            http_response = self.create_Response(method, req_file)
+            
+            clientSocket.send(http_response)
+            
+        except Exception as e:
+            self.log_message(f"[ERROR] Problem with client {clientAddr}: {e}")
+        finally:
+            clientSocket.close()
             
 
     def handle_Client(self):
@@ -63,53 +156,22 @@ class server:
                     break
                 
                 clientSocket, clientAddr = client_data
-        
-                self.log_message(f"[NEW CONNECTION] Client {clientAddr} connected.")
+                self.log_message(f"Connection dequeued, assigned to Thread-{self._THREADPOOL - self._thread_semaphore._value + 1}")
                 
                 available = self._thread_semaphore.acquire(blocking=False)
                 
                 if not available:
-                    self._log_message("[WARNING] Thread pool full but connection was dequeued!")
                     self._thread_semaphore.acquire()
                 
-                if self._thread_semaphore._value == 0:  # No more available slots
-                    self._log_message(f"[POOL STATUS] Thread pool is now FULL! (0/{self._THREADPOOL} available)")
+                if self._thread_semaphore._value == 0: 
+                    self._log_message(f"Warning: Thread pool saturated, queuing connection")
                     
                 try:
-                    try:
-                        request_data = clientSocket.recv(1024)
-                    
-                        req, header = self.parse_http_request(request_data)
-                        
-                        try:
-                            with open('./resources/index.html', 'r') as file:
-                                http_content = file.read()
-                        except FileNotFoundError:
-                            http_content = "<html><body><p>File not found</p></body></html>"
-                            self.log_message("[ERROR] file not found!")
-                            
-                        http_header = (
-                            "HTTP/1.1 200 OK\r\n"
-                            "Content-Type: text/html\r\n"
-                            f"Content-Length: {len(http_content)}\r\n"
-                            "\r\n"
-                        )
-                        
-                        http_response = http_header + http_content
-                        
-                        clientSocket.send(http_response.encode(self._FORMAT))
-
-                    except Exception as e:
-                        self.log_message(f"[ERROR] Problem with client {clientAddr}: {e}")
-                    finally:
-                        clientSocket.close()
-                        self.log_message(f"[CLOSED] Connection with {clientAddr} closed")
+                    self.handle_Request(clientSocket, clientAddr)
                 finally:
                     self._thread_semaphore.release()
-                    self.log_message(f"[POOL STATUS] Thread pool now has availability (1/{self._THREADPOOL} available")  
-            except Exception as e:
-                self.log_message(f"[ERROR] Worker thread error: {e}")
-                
+                    self.log_message(f"Thread pool status: [{self._thread_semaphore._value}/{self._THREADPOOL}] available")  
+            except Exception as e:                
                 if 'available' in locals() and available:
                     self._thread_semaphore.release()
                     
@@ -119,7 +181,11 @@ class server:
             serverSocket.bind(self._ADDR)
             
             serverSocket.listen(50)
-            print(f"[LISTENING] Server is listening on {self._SERVER}:{self._PORT}")
+            self.log_message(f"HTTP Server started on http://{self._SERVER}:{self._PORT}")
+            self.log_message(f"Thread pool size: {self._THREADPOOL}")
+            self.log_message(f"Serving files from 'resources' directory")
+            
+            self.log_message(f"Press Ctrl+C to stop the server")
             
             with concurrent.futures.ThreadPoolExecutor(max_workers = self._THREADPOOL) as executor:
                 for _ in range(self._THREADPOOL):
@@ -130,20 +196,20 @@ class server:
                         clientSocket, clientAddr = serverSocket.accept()
                         
                         self._connection_que.put((clientSocket, clientAddr))
-                        self.log_message(f"[QUEUED] New client {clientAddr} added to queue")
-                        
+                                                
                 except KeyboardInterrupt:
-                    self.log_message("[STOPPING] Received shutdown signal")
+                    self.log_message("Received shutdown signal")
                 finally:
                     for _ in range(self._THREADPOOL):
                         self._connection_que.put(None)
                         
         except Exception as e:
-            self.log_message(f"[CRITICAL ERROR] Server couldn't start: {e}")
+            self.log_message(f"Server couldn't start: {e}")
             
         finally:
             if 'serverSocket' in locals():
                 serverSocket.close()
+                
             
 server = server()
 server.start_Server()
